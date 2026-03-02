9e44efe27811f838168a9e2cbdc83745560a373b
Jean-Francois Alix <alix.jean-francois2@hydroquebec.com>
2026-03-02 10:58:49 -0500

    Added TimescaleDB table creation for Telegraf

--- telegraf/CHANGELOG.md
+++ telegraf/CHANGELOG.md
@@ -1,5 +1,8 @@
 <!-- https://developers.home-assistant.io/docs/add-ons/presentation#keeping-a-changelog -->
 
+## 1.0.2
+
+- Added TimescaleDB table creation
 
 ## 1.0.1
 
--- telegraf/run.sh
+++ telegraf/run.sh
@@ -8,6 +8,27 @@ export BUILDING_ID
 
 CONF="$(bashio::config 'telegraf_config_path')"
 
+# DB connection params (same as telegraf.conf)
+PGHOST="77b2833f-timescaledb"
+PGUSER="postgres"
+PGPASSWORD="homeassistant"
+PGDATABASE="homeassistant"
+export PGHOST PGUSER PGPASSWORD PGDATABASE
+
+bashio::log.info "Initializing TimescaleDB schema (public.space_heating)…"
+psql <<'SQL'
+CREATE TABLE IF NOT EXISTS public.space_heating (
+  time        TIMESTAMPTZ NOT NULL,
+  device_id   TEXT,
+  metric_type TEXT,
+  name        TEXT,
+  value       DOUBLE PRECISION
+);
+
+-- If TimescaleDB is present, this will do nothing if the hypertable already exists
+SELECT create_hypertable('public.space_heating', 'time', if_not_exists => TRUE);
+SQL
+
 bashio::log.info "Starting the custom Telegraf add-on with config ${CONF}"
 telegraf --config "$CONF" --watch-config poll
 
